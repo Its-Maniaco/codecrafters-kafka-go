901d66d6a1555df50a910ef34ba467438fbc244d
Its-Maniaco <21123621+Its-Maniaco@users.noreply.github.com>
2026-01-03 21:07:14 +0530

    parse correlation id

--- app/internal/models.go
+++ app/internal/models.go
@@ -1,19 +1,26 @@
 package internal
 
 type KafkaMessage struct {
-	MessageSize int32
-	Header      Headers
+	MessageSize int32   `json:"message_size"`
+	Header      Headers `json:"header"`
+	Body        string  `json:"body"`
 }
 
 type Headers struct {
-	CorrelationID int32
+	RequestAPIKey     int16   `json:"request_api_key"`
+	RequestAPIVersion int16   `json:"request_api_version"`
+	CorrelationID     int32   `json:"correlation_id"`
+	ClientID          *string `json:"client_id,omitempty"` // nullable string; Go does not allow null for string.
+	TagBuffer         any     `json:"TAG_BUFFER,omitempty"`
 }
 
 func NewKafkaMessage() KafkaMessage {
 	return KafkaMessage{
 		MessageSize: 32,
 		Header: Headers{
-			CorrelationID: 7,
+			RequestAPIKey:     2,
+			RequestAPIVersion: 2,
+			CorrelationID:     4,
 		},
 	}
 }
--- app/main.go
+++ app/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"encoding/binary"
 	"fmt"
+	"log"
 	"net"
 	"os"
 
-	internal "github.com/codecrafters-io/kafka-starter-go/app/internal"
+	"github.com/codecrafters-io/kafka-starter-go/app/internal"
 )
 
 func main() {
@@ -32,13 +34,128 @@ func main() {
 
 func handleClient(conn net.Conn) {
 	defer conn.Close()
+	// read incoming header
+	buf := make([]byte, 1024)
+	n, _ := conn.Read(buf) // to only read the read bytes and not pad end with 0
 
-	buf := make([]byte, 8)
-	msg := internal.NewKafkaMessage()
+	// Parse request into Message
+	msg, err := parseKafkaMessage(buf[:n])
+	if err != nil {
+		log.Println("error parsing kafka message: ", err)
+		conn.Write([]byte(err.Error()))
+		return
+	}
 
 	// struct cannot be written directly to sockets.
 	// Kafka uses binary encoding (so we wont use json.Marshal)
-	binary.Encode(buf, binary.BigEndian, msg)
+	// also this is wrong because binary.Encode() cannot encode strings or any type, it only encodes fixed size types.
+	// below will fail silently:
 
-	conn.Write(buf)
+	/*
+		responseBuf := make([]byte, 1024)
+		n, _ = binary.Encode(responseBuf, binary.BigEndian, msg)
+	*/
+
+	// Manual Encoding: since we have to handle strings and we dont know the final size required
+	// buffer will grow and help us
+	resp := bytes.Buffer{}
+	// skip all fields as we only need correlation id to be sent back
+	binary.Write(&resp, binary.BigEndian, msg.MessageSize)
+	binary.Write(&resp, binary.BigEndian, msg.Header.CorrelationID)
+
+	conn.Write(resp.Bytes())
+}
+
+func parseKafkaMessage(req []byte) (internal.KafkaMessage, error) {
+	msg := internal.NewKafkaMessage()
+
+	reader := bytes.NewReader(req)
+
+	// Read fixed sized fields
+	err := binary.Read(reader, binary.BigEndian, &msg.MessageSize)
+	if err != nil {
+		return msg, err
+	}
+	err = binary.Read(reader, binary.BigEndian, &msg.Header.RequestAPIKey)
+	if err != nil {
+		return msg, err
+	}
+	err = binary.Read(reader, binary.BigEndian, &msg.Header.RequestAPIVersion)
+	if err != nil {
+		return msg, err
+	}
+	err = binary.Read(reader, binary.BigEndian, &msg.Header.CorrelationID)
+	if err != nil {
+		return msg, err
+	}
+
+	/* Wrong as these fields may not be present/empty
+	err = binary.Read(reader, binary.BigEndian, &msg.Header.ClientID)
+	if err != nil {
+		return msg, err
+	}
+	err = binary.Read(reader, binary.BigEndian, &msg.Header.TagBuffer)
+	if err != nil {
+		return msg, err
+	}
+	*/
+
+	/*
+		Kafka protocol: strings are encoded as:
+		Length prefix (int16 for nullable strings, where -1 means null)
+		String bytes (if length > 0)
+	*/
+
+	// only read if there is more data to read else we will get EOF error
+	if reader.Len() == 0 {
+		return msg, nil
+	}
+
+	// Read nullable string (ClientID)
+	/*
+		From: https://kafka.apache.org/41/design/protocol/#protocol-primitive-types
+		NULLABLE_STRING:
+			For non-null strings, first the length N is given as an INT16.
+			Then N bytes of the character sequence.
+			A null value is encoded with length of -1 and no following bytes.
+	*/
+	var clientIDLength int16 // this will represent the length prefix which tells us if string is present or not
+	if err := binary.Read(reader, binary.BigEndian, &clientIDLength); err != nil {
+		return msg, err
+	}
+
+	if clientIDLength == -1 { // null string has been sent in message
+		msg.Header.ClientID = nil
+	} else if clientIDLength == 0 { // empty string has been sent in message
+		s := ""
+		msg.Header.ClientID = &s
+	} else {
+		clientIDBytes := make([]byte, clientIDLength)
+		// read upto those bits
+		if _, err := reader.Read(clientIDBytes); err != nil {
+			return msg, err
+		}
+		clientIDString := string(clientIDBytes)
+		msg.Header.ClientID = &clientIDString
+	}
+
+	if reader.Len() == 0 {
+		return msg, nil
+	}
+
+	// read TagBuffer
+	var tagBufferLength byte
+	if err := binary.Read(reader, binary.BigEndian, &tagBufferLength); err != nil {
+		return msg, fmt.Errorf("reading tag buffer size: %w", err)
+	}
+
+	if tagBufferLength > 0 {
+		tagBytes := make([]byte, tagBufferLength)
+		if _, err := reader.Read(tagBytes); err != nil {
+			return msg, fmt.Errorf("reading tag buffer: %w", err)
+		}
+		msg.Header.TagBuffer = tagBytes
+	}
+
+	return msg, nil
 }
