b3b0feecb62f6ade094bc88803224e5a680ce665
Its-Maniaco <21123621+Its-Maniaco@users.noreply.github.com>
2026-01-12 09:42:44 +0530

    codecrafters submit [skip ci]

--- app/internal/models.go
+++ app/internal/models.go
@@ -1,30 +1,37 @@
 package internal
 
-type KafkaMessage struct {
+type KafkaRequest struct {
 	MessageSize int32   `json:"message_size"`
 	Header      Headers `json:"header"`
 	Body        string  `json:"body"`
 }
 
 type Headers struct {
-	RequestAPIKey     int16   `json:"request_api_key"`
-	RequestAPIVersion int16   `json:"request_api_version"`
-	CorrelationID     int32   `json:"correlation_id"`
-	ClientID          *string `json:"client_id,omitempty"` // nullable string; Go does not allow null for string.
-	TagBuffer         any     `json:"TAG_BUFFER,omitempty"`
+	RequestAPIKey     int16        `json:"request_api_key"`
+	RequestAPIVersion int16        `json:"request_api_version"`
+	CorrelationID     int32        `json:"correlation_id"`
+	ClientID          *string      `json:"client_id,omitempty"` // nullable string; Go does not allow null for string.
+	TagBuffer         TaggedFields `json:"TAG_BUFFER,omitempty"`
 }
 
-func NewKafkaMessage() KafkaMessage {
-	return KafkaMessage{
-		MessageSize: 32,
-		Header: Headers{
-			RequestAPIKey:     2,
-			RequestAPIVersion: 2,
-			CorrelationID:     4,
-		},
-	}
+func NewKafkaRequest() KafkaRequest {
+	return KafkaRequest{}
 }
 
-type APIVersionsResponse struct {
-	ErrorCode int16 `json:"error_code"`
+type APIVersionsResponseV4 struct {
+	ErrorCode      int16        `json:"error_code"`
+	APIKeys        []APIKeys    `json:"api_keys"`
+	ThrottleTimeMS int32        `json:"throttle_time_ms"`
+	TagBuffer      TaggedFields `json:"TAG_BUFFER,omitempty"`
+}
+
+type APIKeys struct {
+	APIKey     int16        `json:"api_key"`
+	MinVersion int16        `json:"min_version"`
+	MaxVersion int16        `json:"max_version"`
+	TagBuffer  TaggedFields `json:"TAG_BUFFER,omitempty"`
+}
+
+type TaggedFields struct {
+	Data []byte
 }
--- app/main.go
+++ app/main.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/binary"
 	"fmt"
+	"io"
 	"log"
 	"net"
 	"os"
@@ -35,46 +36,84 @@ func main() {
 func handleClient(conn net.Conn) {
 	defer conn.Close()
 	// read incoming header
-	buf := make([]byte, 1024)
-	n, _ := conn.Read(buf) // to only read the read bytes and not pad end with 0
+	// we need to create buffer long enough for message size (1024 can be limiting)
 
+	bufSize := make([]byte, 4)
+	// read first 4 bytes to get message length
+	_, err := io.ReadFull(conn, bufSize)
+	if err != nil {
+		return
+	}
+	msgLen := int32(binary.BigEndian.Uint32(bufSize))
+
+	payload := make([]byte, msgLen)
+	_, err = io.ReadFull(conn, payload)
+	if err != nil {
+		log.Println("Error reading payload:", err)
+		return
+	}
 	// Parse request into Message
-	msg, err := parseKafkaMessage(buf[:n])
+	msg, err := parseKafkaRequest(msgLen, payload)
 	if err != nil {
 		log.Println("error parsing kafka message: ", err)
 		conn.Write([]byte(err.Error()))
 		return
 	}
 
-	// Manual Encoding: since we have to handle strings and we dont know the final size required
-	// buffer will grow and help us
+	// fmt.Println("Kafka Req\t: ", msg)
+
 	resp := bytes.Buffer{}
-	// skip all fields as we only need correlation id to be sent back
-	binary.Write(&resp, binary.BigEndian, msg.MessageSize)
-	binary.Write(&resp, binary.BigEndian, msg.Header.CorrelationID)
 
 	apiResponse := HandleAPIKeys(&msg)
-	if apiResponse == nil {
-		conn.Write([]byte("Unknown API key"))
-		return
-	}
 
-	binary.Write(&resp, binary.BigEndian, apiResponse)
+	switch v := apiResponse.(type) {
+	case internal.APIVersionsResponseV4:
+		// fmt.Println("v:::", v)
+		binary.Write(&resp, binary.BigEndian, msg.Header.CorrelationID)
+		binary.Write(&resp, binary.BigEndian, v.ErrorCode)
+		lenApiKeys := uint64(len(v.APIKeys) + 1)
+		// Write as unsigned varint for compact array
+		var varintBuf [binary.MaxVarintLen64]byte
+		n := binary.PutUvarint(varintBuf[:], lenApiKeys)
+		resp.Write(varintBuf[:n])
+
+		for _, apiKey := range v.APIKeys {
+			binary.Write(&resp, binary.BigEndian, apiKey.APIKey)
+			binary.Write(&resp, binary.BigEndian, apiKey.MinVersion)
+			binary.Write(&resp, binary.BigEndian, apiKey.MaxVersion)
+			if apiKey.TagBuffer.Data == nil {
+				binary.Write(&resp, binary.BigEndian, uint8(0)) // tag buffer expects one byte
+			} else {
+				//TODO: implement if tag buffer is not empty then write it currently we still write 0
+				binary.Write(&resp, binary.BigEndian, uint8(0))
+			}
+		}
+		binary.Write(&resp, binary.BigEndian, v.ThrottleTimeMS)
+		binary.Write(&resp, binary.BigEndian, uint8(0)) // tag_buffer
+
+		// now we know the size of message so we write that
+		msgLen := int32(resp.Len())
+		kafkaResp := bytes.Buffer{}
+		binary.Write(&kafkaResp, binary.BigEndian, msgLen)
+		kafkaResp.Write(resp.Bytes())
+		conn.Write(kafkaResp.Bytes())
+		return
+	default:
+		conn.Write([]byte("Unknown API response type"))
+	}
 
 	conn.Write(resp.Bytes())
 }
 
-func parseKafkaMessage(req []byte) (internal.KafkaMessage, error) {
-	msg := internal.NewKafkaMessage()
+func parseKafkaRequest(msgLen int32, req []byte) (internal.KafkaRequest, error) {
+	fmt.Println("req: ", req)
+	msg := internal.NewKafkaRequest()
+	msg.MessageSize = msgLen
 
 	reader := bytes.NewReader(req)
 
-	// Read fixed sized fields
-	err := binary.Read(reader, binary.BigEndian, &msg.MessageSize)
-	if err != nil {
-		return msg, err
-	}
-	err = binary.Read(reader, binary.BigEndian, &msg.Header.RequestAPIKey)
+	// Read fixed sized fields (headers)
+	err := binary.Read(reader, binary.BigEndian, &msg.Header.RequestAPIKey)
 	if err != nil {
 		return msg, err
 	}
@@ -86,17 +125,6 @@ func parseKafkaMessage(req []byte) (internal.KafkaMessage, error) {
 	if err != nil {
 		return msg, err
 	}
-
-	/* Wrong as these fields may not be present/empty
-	err = binary.Read(reader, binary.BigEndian, &msg.Header.ClientID)
-	if err != nil {
-		return msg, err
-	}
-	err = binary.Read(reader, binary.BigEndian, &msg.Header.TagBuffer)
-	if err != nil {
-		return msg, err
-	}
-	*/
 
 	/*
 		Kafka protocol: strings are encoded as:
@@ -141,36 +169,75 @@ func parseKafkaMessage(req []byte) (internal.KafkaMessage, error) {
 		return msg, nil
 	}
 
-	// read TagBuffer
-	var tagBufferLength byte
-	if err := binary.Read(reader, binary.BigEndian, &tagBufferLength); err != nil {
-		return msg, fmt.Errorf("reading tag buffer size: %w", err)
+	// V4 uses varint so we cannot use binary.Read
+	// WRONG:
+	// // read TagBuffer
+	// var tagBufferLength byte
+	// if err := binary.Read(reader, binary.BigEndian, &tagBufferLength); err != nil {
+	// 	return msg, fmt.Errorf("reading tag buffer size: %w", err)
+	// }
+
+	// if tagBufferLength > 0 {
+	// 	tagBytes := make([]byte, tagBufferLength)
+	// 	if _, err := reader.Read(tagBytes); err != nil {
+	// 		return msg, fmt.Errorf("reading tag buffer: %w", err)
+	// 	}
+	// 	taggedFields := internal.TaggedFields{
+	// 		Data: tagBytes,
+	// 	}
+	// 	msg.Header.TagBuffer = taggedFields
+	// }
+
+	// to read Unsigned Varint for TaggedField (V3+ uses Varint)
+	tagCount, err := binary.ReadUvarint(reader)
+	if err != nil {
+		return msg, fmt.Errorf("failed to read header tag count: %v", err)
 	}
 
-	if tagBufferLength > 0 {
-		tagBytes := make([]byte, tagBufferLength)
-		if _, err := reader.Read(tagBytes); err != nil {
-			return msg, fmt.Errorf("reading tag buffer: %w", err)
+	if tagCount > 0 {
+		// store remaining bytes as is for now
+		tagDataLeft := reader.Len()
+		if tagDataLeft > 0 {
+			data := make([]byte, tagDataLeft)
+			_, err := reader.Read(data)
+			if err != nil {
+				return msg, err
+			}
+
+			// Store the tagCount at the beginning or just the raw data
+			// Usually, you want the raw data that follows the length
+			msg.Header.TagBuffer.Data = data
 		}
-		msg.Header.TagBuffer = tagBytes
 	}
-
 	return msg, nil
 }
 
 // data, error
-func RequestAPIVersion(msg *internal.KafkaMessage) internal.APIVersionsResponse {
-	resp := new(internal.APIVersionsResponse)
+func RequestAPIVersion(msg *internal.KafkaRequest) internal.APIVersionsResponseV4 {
+	resp := new(internal.APIVersionsResponseV4)
 	if msg.Header.RequestAPIVersion >= 0 && msg.Header.RequestAPIVersion <= 4 {
 		resp.ErrorCode = 0
 
 	} else {
 		resp.ErrorCode = 35
 	}
+	apiKey := internal.APIKeys{}
+	apiKey.APIKey = 18
+	apiKey.MinVersion = 0
+	apiKey.MaxVersion = 4
+	apiKey.TagBuffer = internal.TaggedFields{Data: []byte{0}}
+	resp.APIKeys = append(resp.APIKeys, apiKey)
+	apiKey.APIKey = 17
+	apiKey.MinVersion = 1
+	apiKey.MaxVersion = 3
+	apiKey.TagBuffer = internal.TaggedFields{Data: []byte{0}}
+	resp.APIKeys = append(resp.APIKeys, apiKey)
+
+	resp.ThrottleTimeMS = 0
 	return *resp
 }
 
-func HandleAPIKeys(msg *internal.KafkaMessage) any {
+func HandleAPIKeys(msg *internal.KafkaRequest) any {
 	if msg == nil {
 		return nil
 	}
@@ -182,3 +249,5 @@ func HandleAPIKeys(msg *internal.KafkaMessage) any {
 		return nil
 	}
 }
+
+
